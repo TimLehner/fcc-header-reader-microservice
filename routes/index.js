9f64b233b627bdef2200480e384c91da39cbf182
Tim Lehner <timothy.lehner.2011@live.rhul.ac.uk>
2017-03-12 16:26:59 +0000

    Fixed getting IP address

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var getIP = require('ipware')().get_ip;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var ip = req.headers.host;
+  var ip = getIP(req).clientIp;
   var os = req.headers['user-agent'].split("(")[1].split(")")[0];
   var language = req.headers['accept-language'].split(",")[0];
   res.end(JSON.stringify({
